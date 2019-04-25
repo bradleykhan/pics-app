0b490ad8958677c80626aa77572144f78978abd8
Bradley Khan <github@bradleykhan.com>
2019-04-25 21:11:32 +0100

    Implement simple search in form submission callback

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,33 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get("https://api.unsplash.com/search/photos", {
-            params: { query: term },
-            headers: {
-                Authorization:
-                    "Client-ID 9cd84eb81636fe08f99180eff0bdd2733cb39654cf39bd4f60e538b141862b98"
+    constructor() {
+        super();
+        this.state = { images: [] };
+
+        this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    }
+
+    async onSearchSubmit(term) {
+        const response = await axios.get(
+            "https://api.unsplash.com/search/photos",
+            {
+                params: { query: term },
+                headers: {
+                    Authorization:
+                        "Client-ID 9cd84eb81636fe08f99180eff0bdd2733cb39654cf39bd4f60e538b141862b98"
+                }
             }
-        });
+        );
+
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: "10px" }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
