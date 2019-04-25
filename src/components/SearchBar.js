c6eebd50c03279afbbf80dab2de0e3b1cb57e03e
Bradley Khan <github@bradleykhan.com>
2019-04-25 19:40:04 +0100

    Refactor App.js to class component, define callback to use to retrieve child component search term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,14 @@ class SearchBar extends React.Component {
 
         this.state = { term: "" };
 
+        this.onFormSubmit = this.onFormSubmit.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     }
 
     onInputChange(event) {
@@ -16,7 +23,7 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input
