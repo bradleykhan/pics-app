7b969dff2c35f8798333b7fcfe1341ffbc0b4fa8
Bradley Khan <github@bradleykhan.com>
2019-04-25 17:19:20 +0100

    Introduce controlled state in SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    constructor() {
+        super();
+
+        this.state = { term: "" };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
